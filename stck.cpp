b1152b3e0859ee6f1aac3cdecd439feb2837f371
MdAamir-Hussain <mdaamirhussain319@gmail.com>
2023-09-12 13:36:04 +0530

    stack-heap

---
+++ stck.cpp
@@ -0,0 +1,39 @@
+// #include<iostream>
+// using namespace std;
+// int add(int num1, int num2)
+// {
+//     return num1+num2;
+// }
+
+// int main() 
+// {
+//     int a=2; int b=3;
+//     int sum = add(a,b);
+//     cout<<sum<<endl;
+//     return 0;
+
+// }
+
+
+
+/*
+Heap
+*/
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int a = 10;     //stored in stack
+    int *p = new int();     //allocate memory in heap
+    *p = 10;        
+    delete(p);      //deallocate memory
+
+    p = new int[4];
+    delete[]p;
+
+    p = NULL;
+    
+    return 0;
+
+}
